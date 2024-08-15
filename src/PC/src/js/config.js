bd87bc499f4ba355ea1b20e5506ffc0697756ad4
JoTa <145311605+jota-js@users.noreply.github.com>
2024-08-15 05:14:53 -0300

    Create config.js

---
+++ src/PC/src/js/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    scriptNumber: 2,
+    TimeToQuit: 20000,
+    TimeToExec: 1000,
+    SiteToExec: "https://google.com/"
+  };
+
+// 1 >> Auto Farm (Cookie Clicker)
+// 2 >> Execute Personalized Script
+
+// https://discord.gg/9QDNKyYKFk
